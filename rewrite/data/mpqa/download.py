68339f3d565cbe5f7576d535ecdf6f0db15dc967
kyle <kyle@wrtn.io>
2025-09-04 19:23:35 +0900

    fix

--- rewrite/data/mpqa/download.py
+++ rewrite/data/mpqa/download.py
@@ -1,20 +1,29 @@
 from datasets import load_dataset
-import pandas as pd
 
-ds = load_dataset("jxm/mpqa")
+ds = load_dataset("SetFit/TREC-QC")
+
+
+import pandas as pd
+import random
 
 if __name__ == "__main__":
     test = []
     train = []
+    valid = []
 
     for example in ds["test"]:
-        test.append((example["label"], example["sentence"]))
+        test.append((example["label"], example["text"]))
 
     for example in ds["train"]:
-        train.append((example["label"], example["sentence"]))
+        train.append((example["label"], example["text"]))
 
-    df_train = pd.DataFrame(train, columns=["label", "sentence"])
-    df_test = pd.DataFrame(test, columns=["label", "sentence"])
+    for example in ds["validation"]:
+        valid.append((example["label"], example["text"]))
+
+    df_train = pd.DataFrame(train, columns=["label", "text"])
+    df_test = pd.DataFrame(test, columns=["label", "text"])
+    df_valid = pd.DataFrame(valid, columns=["label", "text"])
 
     df_train.to_csv("train.csv", index=False, header=False)
     df_test.to_csv("test.csv", index=False, header=False)
+    df_valid.to_csv("validation.csv", index=False, header=False)
