82c2ef9d91eee56a3a0e968669bac4909ff7c521
kyle <kyle@wrtn.io>
2025-09-04 18:18:32 +0900

    fix

--- rewrite/data/subj/download.py
+++ rewrite/data/subj/download.py
@@ -1,6 +1,8 @@
 # subjectivity dataset v1.0
 # http://www.cs.cornell.edu/people/pabo/movie-review-data/rotten_imdb.tar.gz
 
+import random
+import pandas as pd
 import os
 import tarfile
 import urllib.request
@@ -14,3 +16,34 @@ if __name__ == "__main__":
     tar.extractall(path="./")
     tar.close()
     os.remove("rotten_imdb.tar.gz")
+
+    data = []
+    # subjective
+    with open("quote.tok.gt9.5000", "r", encoding="latin-1") as f:
+        for line in f:
+            sentence = line.strip()
+            data.append((1, sentence[1]))
+
+    # objective
+    with open("plot.tok.gt9.5000", "r", encoding="latin-1") as f:
+        for line in f:
+            sentence = line.strip()
+            data.append((0, sentence[1]))
+
+    test = []
+    train = []
+
+    for i in range(len(data)):
+        ran = random.randint(0, 9)
+        if ran == 0:
+            test.append(data[i])
+        else:
+            train.append(data[i])
+
+    df_train = pd.DataFrame(train, columns=["label", "sentence"])
+    df_test = pd.DataFrame(test, columns=["label", "sentence"])
+
+    df_train.to_csv("train.csv", index=False, header=False)
+    df_test.to_csv("test.csv", index=False, header=False)
+
+# http://www.cs.cornell.edu/people/pabo/movie-review-data/rotten_imdb.tar.gz
