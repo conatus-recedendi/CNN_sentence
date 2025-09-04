51774d410fa9301f7f8fa17371b5d2627188aa52
kyle <kyle@wrtn.io>
2025-09-04 16:46:56 +0900

    fix

--- rewrite/data/cr/donwload.py
+++ rewrite/data/cr/donwload.py
@@ -9,38 +9,24 @@
 
 import pandas as pd
 import random
+import os
+import urllib.request
+import zipfile
 
 if __name__ == "__main__":
     print("Downloading and extracting the MR dataset...")
+    url = "https://www.cs.uic.edu/~liub/FBS/CustomerReviewData.zip"
+
+    urllib.request.urlretrieve(url, "CustomerReviewData.zip")
+    with zipfile.ZipFile("CustomerReviewData.zip", "r") as zip_ref:
+        zip_ref.extractall(".")
+    os.remove("CustomerReviewData.zip")
+
+    # "
 
     # read from ./rt-polarity.neg and ./rt-polarity.pos
-
-    # save to ./cr.csv
-    pos_file = "rt-polarity.pos"
-    neg_file = "rt-polarity.neg"
-    data = []
-    labels = []
-    with open(pos_file, "r", encoding="cp1252") as f:
-        for line in f:
-            data.append(line.strip())
-            labels.append(1)
-    with open(neg_file, "r", encoding="cp1252") as f:
-        for line in f:
-            data.append(line.strip())
-            labels.append(0)
-
-    test = []
-    train = []
-
-    for i in range(len(data)):
-        ran = random.randint(0, 9)
-        if ran == 0:
-            test.append((labels[i], data[i]))
-        else:
-            train.append((labels[i], data[i]))
-
-    df_train = pd.DataFrame(train, columns=["label", "sentence"])
-    df_test = pd.DataFrame(test, columns=["label", "sentence"])
-
-    df_train.to_csv("cr_train.csv", index=False, header=False)
-    df_test.to_csv("cr_test.csv", index=False, header=False)
+    # https://www.cs.uic.edu/~liub/FBS/CustomerReviewData.zip
+    # unzip it
+    # then split it into train and test
+    # then read the two files
+    # 10% for test, 90% for train
