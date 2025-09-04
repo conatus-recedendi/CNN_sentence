f5b35fcd0f329cc71cea2bcee4d6c0245d261c0a
kyle <kyle@wrtn.io>
2025-09-04 19:58:52 +0900

    fix

--- rewrite/scripts/p1_table2_train.sh
+++ rewrite/scripts/p1_table2_train.sh
@@ -17,7 +17,19 @@ cd ..
 for d in ${DATA[@]}; do
     echo "Processing $d dataset..."
     python process_data.py GoogleNews-vectors-negative300.bin $d.p ./data/$d/validation.csv ./data/$d/train.csv
+
+    echo "CNN-rand"
+    python conv_net_sentence.py $d.p -nonstatic -rand
+    echo "CNN-static"
+    python conv_net_sentence.py $d.p -nonstatic -word2vec
+    echo "CNN-non-static"
+    python conv_net_sentence.py $d.p -static -word2vec
+    echo "CNN-multichannel"
+    # not impmlemtend
+    # python conv_net_sentence.py $d.p -static -rand
 done
+
+
 
 
 
