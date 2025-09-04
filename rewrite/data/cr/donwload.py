1eebe2a3011d9ea08785553787ca50cf1e91b8e5
kyle <kyle@wrtn.io>
2025-09-04 16:56:04 +0900

    fix

--- rewrite/data/cr/donwload.py
+++ rewrite/data/cr/donwload.py
@@ -1,35 +1,27 @@
-# empty
-
-
-# read rt-polarity.neg
-# read rt-polarity.pos
-
-# data,label 꼴의 csv로 만들어야 함.
-# 또한 단일 파일로 저장하되, train/test로 나누어야 함
-
-import pandas as pd
-import random
 import os
 import urllib.request
-import zipfile
+import rarfile  # pip install rarfile
 
 if __name__ == "__main__":
-    print("Downloading and extracting the MR dataset...")
     url = "http://www.cs.uic.edu/~liub/FBS/opinion-lexicon-English.rar"
+    local_path = "CustomerReviewData.rar"
 
-    urllib.request.urlretrieve(url, "CustomerReviewData.rar")
-    with zipfile.ZipFile("CustomerReviewData.rar", "r") as zip_ref:
-        zip_ref.extractall(".")
-    os.remove("CustomerReviewData.rar")
+    print("Downloading and extracting the CR dataset...")
 
-    # ./CustomerReviewData/negative-words.txt
-    # ./CustomerReviewData/positive-words.txt
+    try:
+        # 다운로드
+        urllib.request.urlretrieve(url, local_path)
+        print("Download completed!")
 
-    # "
+        # RAR 압축 해제
+        with rarfile.RarFile(local_path, "r") as rf:
+            rf.extractall("./CustomerReviewData")
+        print("Extraction completed!")
 
-    # read from ./rt-polarity.neg and ./rt-polarity.pos
-    # https://www.cs.uic.edu/~liub/FBS/CustomerReviewData.zip
-    # unzip it
-    # then split it into train and test
-    # then read the two files
-    # 10% for test, 90% for train
+    except Exception as e:
+        print(f"[ERROR] Failed to download or extract: {e}")
+
+    finally:
+        # 임시 파일 제거
+        if os.path.exists(local_path):
+            os.remove(local_path)
