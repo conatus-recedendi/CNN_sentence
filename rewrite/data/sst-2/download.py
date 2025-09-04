285bd463707d491d9dde8c8613479c058e3a3384
kyle <kyle@wrtn.io>
2025-09-04 16:25:17 +0900

    fix

--- rewrite/data/sst-2/download.py
+++ rewrite/data/sst-2/download.py
@@ -1,4 +1,17 @@
 # https://huggingface.co/datasets/stanfordnlp/sst2
 from datasets import load_dataset
+import pandas as pd
 
-ds = load_dataset("stanfordnlp/sst2")
+
+if __name__ == "__main__":
+    dataset = load_dataset("stanfordnlp/sst2")
+    dataset = dataset["train"].train_test_split(test_size=0.1, seed=42)
+    dataset["validation"] = dataset["test"]
+    del dataset["test"]
+    for split in ["train", "test", "validation"]:
+        df = dataset[split].to_pandas()
+        df = df.drop(0)
+        df = df[["label", "sentence"]]
+        # 2개로 분류
+        df["label"] = pd.cut(df["label"], bins=[0.0, 0.5, 1.0], labels=[0, 1])
+        df.to_csv(f"sst_{split}.csv", index=False, header=False)
