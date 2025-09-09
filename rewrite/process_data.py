a4c3297ea327d193d1623574a49ba96ddfd32701
kyle <kyle@wrtn.io>
2025-09-09 17:14:50 +0900

    fix

--- rewrite/process_data.py
+++ rewrite/process_data.py
@@ -190,8 +190,8 @@ if __name__ == "__main__":
     w2v = load_bin_vec(w2v_file, vocab)
 
     # w2v's variance
-    # variance = np.var(list(w2v.values()))
-    variance = get_variance_of_word_vectors(w2v_file)
+    variance = np.var(list(w2v.values()))
+    # variance = get_variance_of_word_vectors(w2v_file)
     print(f"word2vec loaded! (variance: {variance})")
     print("word2vec loaded!")
     print("num words already in word2vec: " + str(len(w2v)))
